f07d361e62b48d7644c9adac3cf4b1e84e0c2e2a
Daniel J Montanez <dmontanez86@gmail.com>
2016-02-18 19:48:53 -0800

    Baseline commit
    Adding files previously worked on.

---
+++ WebContent/js/app/routes.js
@@ -0,0 +1,70 @@
+angular.module('app')
+    .config(['$stateProvider', '$urlRouterProvider',
+            function($stateProvider, $urlRouterProvider) {
+
+                'use strict';
+
+
+                //Route varibles
+                var home,
+                    education,
+                    experience,
+                    skills,
+                    projects,
+                    other,
+                    resume,
+                    aboutMe,
+                    contact,
+                    socialProfiles;
+
+                home            = 'templates/states/home';
+                education       = 'templates/states/education';
+                experience      = 'templates/states/experience';
+                skills          = 'templates/states/skills';
+                projects        = 'templates/states/projects';
+                other           = 'templates/states/other';
+                resume          = 'templates/states/resume';
+                aboutMe         = 'templates/states/about-me';
+                contact         = 'templates/states/contact';
+                socialProfiles  = 'templates/states/social-profiles';
+
+
+                $urlRouterProvider.otherwise('/home');
+
+                $stateProvider
+                    .state('home', {
+                        url: '/home',
+                        views: {
+                            '': {
+                                templateUrl: home + 'index.html'
+                            }
+                        }
+                    })
+                    .state('education', {
+                        url: '/education'
+                    })
+                    .state('experience', {
+                        url: '/experience'
+                    })
+                    .state('skills', {
+                        url: '/skills'
+                    })
+                    .state('projects', {
+                        url: '/projects'
+                    })
+                    .state('other', {
+                        url: '/other'
+                    })
+                    .state('resume', {
+                        url: '/resume'
+                    })
+                    .state('about-me', {
+                        url: '/about-me'
+                    })
+                    .state('contact', {
+                        url: '/contact'
+                    })
+                    .state('social-profiles', {
+                        url: '/social-profiles'
+                    });
+}]);
